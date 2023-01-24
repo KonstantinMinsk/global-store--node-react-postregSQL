4c62b2e4772fad897a06b0f0085a2ae7621d2f64
Konstantin <sokolov.k007@yandex.ru>
2023-01-24 16:10:16 +0300

    feat: Created checkRoleMiddleware

--- server/routes/typeRoutes.js
+++ server/routes/typeRoutes.js
@@ -1,8 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const typeController = require('../controllers/typeController')
+const checkRole = require('../middleware/checkRoleMiddleware/')
 
-router.post('/', typeController.create)
+const roles = ['ADMIN']
+
+router.post('/', checkRole(roles[0]), typeController.create)
 router.get('/', typeController.getAll)
 
 module.exports = router
